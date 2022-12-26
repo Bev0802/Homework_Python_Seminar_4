f01fff7e80e1f8a4bc4e825420eda7ae4344a09e
Katy <bev08021982@gmail.com>
2022-12-26 23:35:51 +0400

    Решила задачу 32

--- Task_32.py
+++ Task_32.py
@@ -1,1 +1,29 @@
-'''Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.'''
+'''Задайте последовательность чисел. Напишите программу, которая выведет список
+ неповторяющихся элементов исходной последовательности.'''
+
+#Функция находит повторяющиеся числа и выводит список неповторяющихся элементов исходной последовательности.
+def SearchRepeatNumbers(list_string):
+    lens = len(list_string)
+    i=0
+    k=1
+    while i<lens-1:
+        l = list_string[i]
+        while k<lens-1:
+         k=k+1
+         if l==list_string[k]:
+             m=list_string[k]
+             list_string.remove(m)
+             list_string.remove(l)
+             lens=len(list_string)
+             break
+        i=i+1
+        k=i
+    list_int=list(map(int, list_string))
+    return list_int
+
+list_s = list(input("Введите числа через пробел:\n").split())
+list_i=list(map(int, list_s))
+
+print(f'{list_i} => {SearchRepeatNumbers(list_s)}')
+
+
