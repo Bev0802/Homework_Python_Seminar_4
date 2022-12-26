81b3ea088d9a0e5f5531d3f90b5903f1161c7251
Katy <bev08021982@gmail.com>
2022-12-26 23:53:48 +0400

    Решила задачу 32 двумя способами

--- Task_32.py
+++ Task_32.py
@@ -21,15 +21,16 @@ def SearchRepeatNumbers(list_string):
     list_int=list(map(int, list_string))
     return list_int
 #Функция находит повторяющиеся числа в числовом списке и выводит список уникальных неповторяющихся элементов исходной последовательности.
-def SearchRepeat(lst):
+def SearchRepeat(lst_int):
     new_lst = []
-    [new_lst.append(i) for i in lst if i not in new_lst]
+    [new_lst.append(i) for i in lst_int if i not in new_lst]
     return new_lst
 
-
+#Программа
 list_s = list(input("Введите числа через пробел:\n").split())
-
 list_i=list(map(int, list_s))
+
+#Вывод результата
 print(f'Первый вариант решения:{list_i} => {SearchRepeatNumbers(list_s)}')
 print(f'Второй вариант решения:{list_i} => {SearchRepeat(list_i)}')
 
