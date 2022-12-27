ca99d85e99ad4a62772b9cd3d19dcd6c45d23591
Katy <bev0821982@gmail.com>
2022-12-27 14:17:29 +0400

    Решила задачу 32 двумя способами

--- Task_32.py
+++ Task_32.py
@@ -1,33 +1,28 @@
 '''Задайте последовательность чисел. Напишите программу, которая выведет список
  неповторяющихся элементов исходной последовательности.'''
-
-#Функция находит повторяющиеся числа в строковом списке и выводит список неповторяющихся элементов исходной последовательности.
+# ФУНКЦИИ
+# Функция находит повторяющиеся числа в строковом списке и выводит список неповторяющихся элементов.
 def SearchRepeatNumbers(list_string):
-    new_lst=[]
-    i=0
-    while i<len(list_string):
-      l = list_string[i]
-      count = list_string.count(l)
-      if count==1:
-        new_lst.append(l)
-      i=i+1      
-    list_int=list(map(int, new_lst))
+    new_lst = []
+    i = 0
+    while i < len(list_string):
+        l = list_string[i]
+        if (list_string.count(l)) == 1:
+            new_lst.append(l)
+        i = i+1
+    list_int = list(map(int, new_lst))
     return list_int
 
-#Функция находит повторяющиеся числа в числовом списке и выводит список уникальных неповторяющихся элементов исходной последовательности.
+# Функция находит повторяющиеся числа в числовом списке и выводит список уникальных неповторяющихся элементов исходной последовательности.
 def SearchRepeat(lst_int):
     new_lst = []
     [new_lst.append(i) for i in lst_int if i not in new_lst]
     return new_lst
 
-#Программа
+# ВВОД ДАННЫХ
 list_s = list(input("Введите числа через пробел:\n").split())
-list_i=list(map(int, list_s))
+list_i = list(map(int, list_s))
 
-print(list_new:= set(list_s))
-
-#Вывод результата
+# ВЫВОД РЕЗУЛЬТАТА
 print(f'Первый вариант решения:{list_i} => {SearchRepeatNumbers(list_s)}')
 print(f'Второй вариант решения:{list_i} => {SearchRepeat(list_i)}')
-
-
